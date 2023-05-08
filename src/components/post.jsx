afdd5154f547b2dde3e41b4095dcf6246963c95f
Bansharee Ireen <81589236+banshee56@users.noreply.github.com>
2023-05-08 16:43:56 -0400

    update works without pre-existing values

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -29,21 +29,42 @@ export default function Post(props) {
     title: post.title, content: post.content, coverUrl: post.coverURL, tags: post.tags,
   };
 
-  const [updated, setUpdated] = useState(initialPost);
+  // const [updated, setUpdated] = useState(initialPost);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    console.log(name);
-    console.log(value);
-    setUpdated({
-      ...updated,
-      [name]: value,
-    });
-  };
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   console.log(name);
+  //   console.log(value);
+  //   setUpdated({
+  //     ...updated,
+  //     [name]: value,
+  //   });
+  // };
 
   const updateTitle = (e) => {
     const fields = {
-      title: e.target.value, content: updated.content, coverUrl: updated.coverURL, tags: updated.tags,
+      title: e.target.value, content: post.content, coverUrl: post.coverURL, tags: post.tags,
+    };
+    dispatch(updatePost(fields, postID));
+  };
+
+  const updateContent = (e) => {
+    const fields = {
+      title: post.title, content: e.target.value, coverUrl: post.coverURL, tags: post.tags,
+    };
+    dispatch(updatePost(fields, postID));
+  };
+
+  const updateCover = (e) => {
+    const fields = {
+      title: post.title, content: post.content, coverUrl: e.target.value, tags: post.tags,
+    };
+    dispatch(updatePost(fields, postID));
+  };
+
+  const updateTags = (e) => {
+    const fields = {
+      title: post.title, content: post.content, coverUrl: post.coverUrl, tags: e.target.value,
     };
     dispatch(updatePost(fields, postID));
   };
@@ -58,25 +79,25 @@ export default function Post(props) {
         <form className="column-container">
           <label htmlFor="name">
             Name:
-            <input type="text" value={post.title} className="textbox" name="title" onChange={updateTitle} />
+            <input type="text" className="textbox" name="title" onChange={updateTitle} />
           </label>
 
           <label htmlFor="photo-url">
             Photo URL:
-            <input type="text" className="textbox" name="coverURL" onChange={handleInputChange} />
+            <input type="text" className="textbox" name="coverURL" onChange={updateCover} />
           </label>
 
           <label htmlFor="tags">
             Tags:
-            <input type="text" className="textbox" name="tags" onChange={handleInputChange} />
+            <input type="text" className="textbox" name="tags" onChange={updateTags} />
           </label>
 
-          <label htmlFor="tags">
+          <label htmlFor="content">
             Description:
             <textarea
               name="content"
               className="content-textbox"
-              onChange={handleInputChange}
+              onChange={updateContent}
             />
           </label>
 
